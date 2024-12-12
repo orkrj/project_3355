b1e8b826e9fb2e7165c88bcf51de36d941ca158a
= <=>
2024-12-12 10:53:34 +0900

    setting: project init

---
+++ src/main/java/elice/webshopping/WebshoppingApplication.java
@@ -0,0 +1,13 @@
+package elice.webshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshoppingApplication.class, args);
+    }
+
+}
