bdf8ecb733a6c6f9ceda3d6064601cb8fba005e4
practicaluser <dlcnsdncjswo@naver.com>
2024-12-12 13:56:00 +0900

    setup

---
+++ src/main/java/elice/webshopping/service/order/OrderRepository.java
@@ -0,0 +1,9 @@
+package elice.webshopping.service.order;
+
+import elice.webshopping.domain.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ src/test/java/elice/webshopping/WebshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package elice.webshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
