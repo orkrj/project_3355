98326ab3e3532d92fd406a875c2f26cf5600b3ce
= <=>
2024-12-12 12:47:22 +0900

    setup with security permit-all

---
+++ src/main/java/elice/webshopping/config/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package elice.webshopping.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
