123ae07626b6ec2f5a65a14418677309f2ffd087
zun <k5orkrj@gmail.com>
2024-12-13 15:24:20 +0900

    feat: add order-service-interface

--- src/main/java/elice/webshopping/service/order/OrderService.java
+++ src/main/java/elice/webshopping/service/order/OrderService.java
@@ -1,15 +1,29 @@
 package elice.webshopping.service.order;
 
-import elice.webshopping.repository.order.OrderRepository;
-import lombok.RequiredArgsConstructor;
+import elice.webshopping.domain.order.Order;
+import elice.webshopping.domain.order.OrderRequestDto;
+import elice.webshopping.domain.order.OrderResponseDto;
 import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class OrderService {
+import java.util.List;
 
-    private final OrderRepository orderRepository;
+public interface OrderService {
 
-    // 주문 조회
+    //== 주문 생성 ==//
+    public OrderResponseDto createOrder(OrderRequestDto orderRequestDto);
 
+    //== 주문 전체 조회 ==//
+    public List<OrderResponseDto> getOrders();
+
+    //== 주문 단건 조회 ==//
+    public OrderResponseDto getOrderById(Long orderId);
+
+    //== 비즈니스 로직 내 사용할 단건 조회 ==//
+    public Order getOrderEntityById(Long orderId);
+
+    //== 주문 수정 ==//
+    public OrderResponseDto updateOrder(Long orderId, OrderRequestDto orderRequestDto);
+
+    //== 주문 삭제 ==//
+    public void deleteOrder(Long orderId);
 }
