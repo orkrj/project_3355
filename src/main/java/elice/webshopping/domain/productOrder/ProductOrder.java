81a1464db82447884a4727a88fcb259f55cd0aef
zun <k5orkrj@gmail.com>
2024-12-13 17:33:06 +0900

    add static method to convert product-order entity into product-order-response-dto

--- src/main/java/elice/webshopping/domain/productOrder/ProductOrder.java
+++ src/main/java/elice/webshopping/domain/productOrder/ProductOrder.java
@@ -29,4 +29,11 @@ public class ProductOrder {
 
     @Column(nullable = false)
     private int price;
+
+    public static ProductOrderResponseDto to(ProductOrder productOrder) {
+        return new ProductOrderResponseDto(
+                productOrder.getProductOrderId(),
+                productOrder.getQuantity()
+        );
+    }
 }
