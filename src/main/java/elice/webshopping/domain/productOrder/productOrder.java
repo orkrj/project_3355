ace9697c6b8b47fae4d3a8bc6043f073226412e2
zun <k5orkrj@gmail.com>
2024-12-13 15:57:52 +0900

    feat: add product-order entity to handle n:m relation mapping

---
+++ src/main/java/elice/webshopping/domain/productOrder/productOrder.java
@@ -0,0 +1,35 @@
+package elice.webshopping.domain.productOrder;
+
+import elice.webshopping.domain.order.Order;
+import elice.webshopping.domain.product.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products_orders")
+@Getter @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class productOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderProductId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private int price;
+}
