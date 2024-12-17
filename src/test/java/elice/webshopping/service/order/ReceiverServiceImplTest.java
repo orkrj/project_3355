a0bec73d6c441beaa89e8a9cf5ddd59f30f88d26
zun <k5orkrj@gmail.com>
2024-12-17 14:13:49 +0900

    test: set-up for ReceiverServiceImpl

---
+++ src/test/java/elice/webshopping/service/order/ReceiverServiceImplTest.java
@@ -0,0 +1,51 @@
+package elice.webshopping.service.order;
+
+import elice.webshopping.repository.order.ReceiverRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReceiverServiceImplTest {
+
+    @Mock
+    private ReceiverRepository receiverRepository;
+
+    @InjectMocks
+    private ReceiverServiceImpl receiverService;
+
+    @Test
+    @DisplayName("")
+    void createReceiver() {
+
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    @DisplayName("")
+    void findReceiverById() {
+
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    @DisplayName("")
+    void updateReceiver() {
+
+        // given
+
+        // when
+
+        // then
+    }
+}
