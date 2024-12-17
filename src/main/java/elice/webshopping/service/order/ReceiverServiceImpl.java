14b21e281ee5f9114acb66a33d5b12f5dbd2c20a
zun <k5orkrj@gmail.com>
2024-12-17 11:15:28 +0900

    add method to find a Receiver

--- src/main/java/elice/webshopping/service/order/ReceiverServiceImpl.java
+++ src/main/java/elice/webshopping/service/order/ReceiverServiceImpl.java
@@ -3,6 +3,7 @@ package elice.webshopping.service.order;
 import elice.webshopping.domain.order.Receiver;
 import elice.webshopping.domain.order.ReceiverRequestDto;
 import elice.webshopping.domain.order.ReceiverResponseDto;
+import elice.webshopping.exception.order.ReceiverNotExistException;
 import elice.webshopping.repository.order.ReceiverRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,8 @@ public class ReceiverServiceImpl implements ReceiverService {
 
     @Override
     public Receiver findReceiverById(Long receiverId) {
-        return null;
+        return receiverRepository.findById(receiverId)
+                .orElseThrow(() -> new ReceiverNotExistException(receiverId));
     }
 
     @Override
