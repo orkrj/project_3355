f71e8209d8354a79d60b23e04c9ec1d2b6452db0
zun <k5orkrj@gmail.com>
2024-12-18 14:15:01 +0900

    feat: setup for order-controller

--- src/main/java/elice/webshopping/controller/order/OrderController.java
+++ src/main/java/elice/webshopping/controller/order/OrderController.java
@@ -1,7 +1,43 @@
 package elice.webshopping.controller.order;
 
-import org.springframework.stereotype.Controller;
+import elice.webshopping.domain.order.OrderRequestDto;
+import elice.webshopping.domain.order.OrderResponseDto;
+import elice.webshopping.service.order.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
+    // TODO 생성 로직 미구현: UserService 필요
+    public ResponseEntity<OrderResponseDto> createOrder(OrderRequestDto orderRequestDto) {
+        return ResponseEntity.ok(orderService.createOrder(orderRequestDto));
+    }
+
+    public ResponseEntity<List<OrderResponseDto>> getOrders() {
+        return ResponseEntity.ok(orderService.getOrders());
+    }
+
+    public ResponseEntity<OrderResponseDto> getOrderById(Long orderId) {
+        return ResponseEntity.ok(orderService.getOrderResponseDtoById(orderId));
+    }
+
+    public ResponseEntity<Void> cancelOrder(Long orderId) {
+        orderService.cancelOrder(orderId);
+        return ResponseEntity.ok().build();
+    }
+
+    public ResponseEntity<Void> deleteOrderForAdmin(Long orderId) {
+        orderService.deleteOrder(orderId);
+        return ResponseEntity.ok().build();
+    }
 }
