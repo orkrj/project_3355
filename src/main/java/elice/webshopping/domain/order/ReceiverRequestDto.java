247bfcd6be03fd3ca1d9b67948ab20a6253a7a31
zun <k5orkrj@gmail.com>
2024-12-18 14:54:09 +0900

    feat: add controllers for receiver

--- src/main/java/elice/webshopping/domain/order/ReceiverRequestDto.java
+++ src/main/java/elice/webshopping/domain/order/ReceiverRequestDto.java
@@ -3,6 +3,16 @@ package elice.webshopping.domain.order;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
+/**
+ *
+ * @param name -> 정규식 패턴 유효성 검증 필요
+ * @param phoneNumber -> 정규식 패턴 유효성 검증 필요
+ * @param zipCode -> 정규식 패턴 유효성 검증 필요
+ * @param streetAddress -> 정규식 패턴 유효성 검증 필요
+ * @param detailAddress -> 정규식 패턴 유효성 검증 필요
+ * @param message -> 길이 제한 필요
+ */
+
 public record ReceiverRequestDto (
         @NotNull @NotEmpty String name,
         @NotNull @NotEmpty String phoneNumber,
