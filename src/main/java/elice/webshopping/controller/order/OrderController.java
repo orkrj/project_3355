326846db620dccaf3db3ca16cd30dcaf8a6f7cff
zun <k5orkrj@gmail.com>
2024-12-18 14:22:29 +0900

    feat: add rest-controller methods for Order

--- src/main/java/elice/webshopping/controller/order/OrderController.java
+++ src/main/java/elice/webshopping/controller/order/OrderController.java
@@ -5,9 +5,7 @@ import elice.webshopping.domain.order.OrderResponseDto;
 import elice.webshopping.service.order.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,24 +17,29 @@ public class OrderController {
     private final OrderService orderService;
 
     // TODO 생성 로직 미구현: UserService 필요
-    public ResponseEntity<OrderResponseDto> createOrder(OrderRequestDto orderRequestDto) {
+    @PostMapping
+    public ResponseEntity<OrderResponseDto> createOrder(@RequestBody OrderRequestDto orderRequestDto) {
         return ResponseEntity.ok(orderService.createOrder(orderRequestDto));
     }
 
+    @GetMapping
     public ResponseEntity<List<OrderResponseDto>> getOrders() {
         return ResponseEntity.ok(orderService.getOrders());
     }
 
-    public ResponseEntity<OrderResponseDto> getOrderById(Long orderId) {
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponseDto> getOrderById(@PathVariable Long orderId) {
         return ResponseEntity.ok(orderService.getOrderResponseDtoById(orderId));
     }
 
-    public ResponseEntity<Void> cancelOrder(Long orderId) {
+    @PatchMapping("/{orderId}")
+    public ResponseEntity<Void> cancelOrder(@PathVariable Long orderId) {
         orderService.cancelOrder(orderId);
         return ResponseEntity.ok().build();
     }
 
-    public ResponseEntity<Void> deleteOrderForAdmin(Long orderId) {
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<Void> deleteOrderForAdmin(@PathVariable Long orderId) {
         orderService.deleteOrder(orderId);
         return ResponseEntity.ok().build();
     }
