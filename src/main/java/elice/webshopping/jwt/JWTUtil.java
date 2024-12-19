bf94ab92a80cbf0d1bcf7fb944d6d5df1cf98dba
= <=>
2024-12-19 15:18:46 +0900

    refactor : jwt secretKey 발급 방법 수정

--- src/main/java/elice/webshopping/jwt/JWTUtil.java
+++ src/main/java/elice/webshopping/jwt/JWTUtil.java
@@ -2,23 +2,33 @@ package elice.webshopping.jwt;
 
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class JWTUtil {
+    /*
     //private SecretKey secretKey;
 
     SecretKey secretKey = Jwts.SIG.HS256.key().build();
-    /* //키 생성이 안됨..
+     //키 생성이 안됨..
     public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
      */
+    private final SecretKey secretKey;
+
+    public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
 
     public String getUsername(String token) {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
