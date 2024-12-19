277cf6e5d30f379ecf51ff61deca0712de2a313f
= <=>
2024-12-19 16:38:40 +0900

    feat: data.sql로 초기값 생성하도록 변경

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+-- 상위 카테고리 삽입
+INSERT INTO categories (category_id, parent_id, name) VALUES (1, NULL, 'Outer');
+INSERT INTO categories (category_id, parent_id, name) VALUES (2, NULL, 'Top');
+INSERT INTO categories (category_id, parent_id, name) VALUES (3, NULL, 'Bottom');
+INSERT INTO categories (category_id, parent_id, name) VALUES (4, NULL, 'ACC');
+
+-- 하위 카테고리 삽입
+INSERT INTO categories (category_id, parent_id, name) VALUES (5, 1, '코트');
+INSERT INTO categories (category_id, parent_id, name) VALUES (6, 1, '자켓');
+INSERT INTO categories (category_id, parent_id, name) VALUES (7, 1, '가디건');
+
+INSERT INTO categories (category_id, parent_id, name) VALUES (8, 2, '티셔츠');
+INSERT INTO categories (category_id, parent_id, name) VALUES (9, 2, '맨투맨');
+
+INSERT INTO categories (category_id, parent_id, name) VALUES (10, 3, '데님');
+INSERT INTO categories (category_id, parent_id, name) VALUES (11, 3, '슬랙스');
+
+INSERT INTO categories (category_id, parent_id, name) VALUES (12, 4, '신발');
