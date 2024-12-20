ee04326b070caf415928892b37e4a1822129e72c
= <=>
2024-12-20 16:27:58 +0900

    feat : 회원 삭제 추가

--- src/main/java/elice/webshopping/repository/user/UserRepository.java
+++ src/main/java/elice/webshopping/repository/user/UserRepository.java
@@ -12,4 +12,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
     boolean existsByPhone(String phone);
+
+    void deleteByUsername(String username);
+
 }
