b8a73bf2c9fbdf59e26549300254c9065cbfa584
zun <k5orkrj@gmail.com>
2024-12-20 15:36:02 +0900

    feat: add price field to ProductOrderRequestDto for per-item pricing

--- src/main/java/elice/webshopping/domain/productOrder/ProductOrderRequestDto.java
+++ src/main/java/elice/webshopping/domain/productOrder/ProductOrderRequestDto.java
@@ -4,5 +4,6 @@ public record ProductOrderRequestDto(
         Long productId,
         // price 는 클라이언트에서 가격 조작할 가능성이 있지 않을까?
         // 할인 등을 적용한다면 가격을 받는 게 좋아보이긴 함
+        int price,
         int quantity
 ) {}
