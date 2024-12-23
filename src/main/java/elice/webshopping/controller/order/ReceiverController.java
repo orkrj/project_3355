077c5ad08234a6f0ce7fe3dd59a0041bd77116c3
zun <k5orkrj@gmail.com>
2024-12-23 16:04:00 +0900

    chore: add 'api' end-point to seperate controller url and static url

--- src/main/java/elice/webshopping/controller/order/ReceiverController.java
+++ src/main/java/elice/webshopping/controller/order/ReceiverController.java
@@ -9,7 +9,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/receiver")
+@RequestMapping("/api/receiver")
 @RequiredArgsConstructor
 public class ReceiverController {
 
