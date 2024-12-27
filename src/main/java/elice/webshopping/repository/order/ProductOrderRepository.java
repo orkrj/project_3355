f7ad95a1e47a8d42f096d0ff85e3371d372d3fd4
zun <k5orkrj@gmail.com>
2024-12-27 17:38:21 +0900

    refactor: separate order and productOrder logic into their own packages

--- src/main/java/elice/webshopping/repository/order/ProductOrderRepository.java
+++ src/main/java/elice/webshopping/repository/order/ProductOrderRepository.java
@@ -1,9 +1,9 @@
-package elice.webshopping.repository.order;
-
-import elice.webshopping.domain.productOrder.ProductOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductOrderRepository extends JpaRepository<ProductOrder, Long> {
-}
+//package elice.webshopping.repository.order;
+//
+//import elice.webshopping.domain.productOrder.ProductOrder;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface ProductOrderRepository extends JpaRepository<ProductOrder, Long> {
+//}
--- src/main/java/elice/webshopping/service/order/ProductOrderService.java
+++ src/main/java/elice/webshopping/service/order/ProductOrderService.java
@@ -1,9 +1,9 @@
-package elice.webshopping.service.order;
-
-import elice.webshopping.domain.productOrder.ProductOrderRequestDto;
-import elice.webshopping.domain.productOrder.ProductOrderResponseDto;
-
-public interface ProductOrderService {
-
-    ProductOrderResponseDto createProductOrder(ProductOrderRequestDto productOrderRequestDto);
-}
+//package elice.webshopping.service.order;
+//
+//import elice.webshopping.domain.productOrder.ProductOrderRequestDto;
+//import elice.webshopping.domain.productOrder.ProductOrderResponseDto;
+//
+//public interface ProductOrderService {
+//
+//    ProductOrderResponseDto createProductOrder(ProductOrderRequestDto productOrderRequestDto);
+//}
