fbf54446d17864d2a5dfd036c3a74d5417240627
zun <k5orkrj@gmail.com>
2024-12-30 11:22:53 +0900

    refactor: modified tests to reflect modified domains

--- src/main/java/elice/webshopping/domain/order/Order.java
+++ src/main/java/elice/webshopping/domain/order/Order.java
@@ -31,10 +31,6 @@ public class Order {
     @Column(nullable = false, unique = true)
     private String orderNumber;
 
-    @Setter
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Payment payment;
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private OrderStatus status;
@@ -66,6 +62,10 @@ public class Order {
     @Setter
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductOrder> productOrders = new ArrayList<>();
+
+    @Setter
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Payment payment;
 
     // @CreatedDate
     @Column(updatable = false, nullable = false)
