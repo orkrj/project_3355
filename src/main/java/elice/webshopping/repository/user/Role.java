9023eb6b935980086a6ab558175fcbb711cbe7c0
= <=>
2025-01-02 17:01:27 +0900

    refactor : Role 수정

--- src/main/java/elice/webshopping/repository/user/Role.java
+++ src/main/java/elice/webshopping/repository/user/Role.java
@@ -4,15 +4,5 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public enum Role {
-    ROLE_ADMIN, ROLE_USER;
-
-    String myRole;
-
-    Role(String myRole) {
-        this.myRole = myRole;
-    }
-
-    public String value(){
-        return myRole;
-    }
+    ROLE_ADMIN, ROLE_USER
 }
