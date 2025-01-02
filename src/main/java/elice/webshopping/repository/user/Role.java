4e7fb15a0396fb519a000a4743922d44d05d0f34
= <=>
2025-01-02 14:14:45 +0900

    feat : 권한 가져오는 메서드 추가

--- src/main/java/elice/webshopping/repository/user/Role.java
+++ src/main/java/elice/webshopping/repository/user/Role.java
@@ -1,5 +1,18 @@
 package elice.webshopping.repository.user;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public enum Role {
-    ROLE_ADMIN, ROLE_USER
+    ROLE_ADMIN, ROLE_USER;
+
+    String myRole;
+
+    Role(String myRole) {
+        this.myRole = myRole;
+    }
+
+    public String value(){
+        return myRole;
+    }
 }
