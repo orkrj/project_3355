370b4804626f1564a026e3067c720d259d2e887f
= <=>
2025-01-03 15:36:14 +0900

    feat: admin-products 삭제기능 구현

--- src/main/resources/static/admin-products/admin-products.js
+++ src/main/resources/static/admin-products/admin-products.js
@@ -107,26 +107,27 @@ async function insertProducts(page = 0, size = 8, sortBy = "createdAt", directio
 }
 
 // 상품 삭제 처리
-async function deleteProductData() {
-  try {
-    console.log("삭제하려는 상품 ID:", productIdToDelete);
+async function deleteProductData(e) {
+  e.preventDefault();
 
-    const response = await Api.delete(`/api/product/${productIdToDelete}`);
-    if (response.ok) {
-      alert("상품이 삭제되었습니다.");
-    } else {
-      throw new Error(`삭제 실패: ${response.status}`);
-    }
+  const response = await fetch(`/api/product/${productIdToDelete}`, { //삭제 진행
+    method: "DELETE"
+  });
+
+  if (response.ok) {
+    alert("상품이 삭제되었습니다.");
 
     const deletedItem = document.querySelector(`#product-${productIdToDelete}`);
-    if (deletedItem) deletedItem.remove();
+    deletedItem.remove();
 
-    productIdToDelete = ""; // 초기화
+    // 전역변수 초기화
+    productIdToDelete = "";
+
     closeModal();
-  } catch (err) {
-    console.error("상품 삭제 중 오류가 발생했습니다:", err);
+  } else {
     alert("상품 삭제 중 문제가 발생했습니다. 다시 시도해주세요.");
   }
+
 }
 
 // 삭제 취소
